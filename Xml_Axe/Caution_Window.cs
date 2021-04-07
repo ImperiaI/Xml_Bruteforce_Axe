a3dadc789f9ea9aad9e7da2a453fc0fcffeb312e
Imperial <primemirror@gmail.com>
2021-04-07 15:28:39 +0200

    Finalized File Selection feature
    - Added a Xml_Directory variable for more convenient access.
    - Fixed little bug where the selected XML is supposed to be loaded, instead the first xml in the row used to load.

--- Xml_Axe/Caution_Window.cs
+++ Xml_Axe/Caution_Window.cs
@@ -15,40 +15,56 @@ using System.Text.RegularExpressions;
 namespace Log_Compactor
 {
     public partial class Caution_Window : Form   
-    {
-        public Caution_Window() { InitializeComponent(); }
-
+    {              
+        Form The_Window = Application.OpenForms["Window"];
+        Window Active_Window = null;
         public bool List_Exclusion_Mode = false;
 
-        
         // Declaring static variable to pass it to the Main_Window Form:
         public static class Passed_Value_A
-        {
-            public static string Text_Data { get; set; }        
-        }
+        { public static string Text_Data { get; set; } }
 
-        public static class Passed_Table
-        {
-            public static List<string> Content { get; set; }
+
+        
+        public Caution_Window() 
+        {        
+            InitializeComponent(); 
         }
- 
 
         private void Caution_Window_Load(object sender, EventArgs e)
         {
+            Active_Window = (Window)The_Window;
             Passed_Value_A.Text_Data = "false";
 
             List_View_Info.Size = new Size(this.Size.Width - 10, this.Size.Height - 10);
         }
       
+    
+  
 
+
+ 
 
 
         public void Button_Caution_Box_1_Click(object sender, EventArgs e)
         {   
             // Setting the passed value to true (its a string because later we might need texts)
             Passed_Value_A.Text_Data = "true";
+
+            if (List_Exclusion_Mode)
+            {   foreach (ListViewItem Item in List_View_Info.Items)
+                {   try 
+                    {   if (Active_Window != null & Item != null)
+                        {   if (Item.Text != "" & !Item.Text.Contains("Are you sure"))
+                            { Active_Window.File_Collection.Add(Active_Window.Xml_Directory + Item.Text); }
+                        }
+                    } catch {}
+                }              
+            }
+
             this.Close();
         }
+
 
         public void Button_Caution_Box_2_Click(object sender, EventArgs e)
         {            
@@ -62,10 +78,7 @@ namespace Log_Compactor
 
             if (!List_Exclusion_Mode) { this.Close(); }
             else
-            {
-                Window Main_Class = new Window();
-                
-                foreach (string Selection in Main_Class.Select_List_View_Items(List_View_Info))
+            {   foreach (string Selection in Active_Window.Select_List_View_Items(List_View_Info))
                 {
                     foreach (ListViewItem Item in List_View_Info.Items)
                     {
@@ -73,15 +86,8 @@ namespace Log_Compactor
                         { List_View_Info.Items.Remove(Item); }
                     }                  
                 }
-            }
-
-
-            foreach (ListViewItem Item in List_View_Info.Items)
-            {
-                if (Item.Text != "" & Item != null) { Passed_Table.Content.Add(Item.Text); }
-            }  
-
-            
+            }    
+  
         }
 
 
--- Xml_Axe/Window.cs
+++ Xml_Axe/Window.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -43,6 +42,8 @@ namespace Log_Compactor
         int Scale_Factor = 10;
         string Tag_List = "";
         string Temporal_A, Temporal_B = "";
+        public string Xml_Directory = Properties.Settings.Default.Xml_Directory;
+        public List<string> File_Collection = new List<string>();
         string[] Tag_Info;
 
         private void Window_Load(object sender, EventArgs e)
@@ -190,7 +191,8 @@ namespace Log_Compactor
                 if (!Regex.IsMatch(Temporal_A, "(?i).*?" + "xml$")) { Temporal_A = Path.GetDirectoryName(Temporal_A); }
                 else if (Regex.IsMatch(Temporal_A, "(?i).*?" + "data$"))
                 {
-                    Properties.Settings.Default.Xml_Directory = Temporal_A + @"\xml";                 
+                    Xml_Directory = Temporal_A + @"\xml\"; // Updating 
+                    Properties.Settings.Default.Xml_Directory = Temporal_A + @"\xml\";                 
                     // Leaping back for a directy, to get the name of the Modpath
                     Properties.Settings.Default.Mod_Directory = Path.GetDirectoryName(Temporal_A); ;
 
@@ -198,9 +200,11 @@ namespace Log_Compactor
                     break;
                 }
 
-                else //Until we goot to the Xml directory
+                else //Until we got to the Xml directory
                 {
-                    Properties.Settings.Default.Xml_Directory = Temporal_A;
+                    Xml_Directory = Temporal_A + @"\"; // Updating 
+                    Properties.Settings.Default.Xml_Directory = Temporal_A + @"\";
+
 
                     // Leaping back by 2 directoies, to get the name of the Modpath
                     Properties.Settings.Default.Mod_Directory = Path.GetDirectoryName(Path.GetDirectoryName(Temporal_A)); ;
@@ -406,33 +410,28 @@ namespace Log_Compactor
             //MessageBox.Show(Properties.Settings.Default.Mod_Directory);
             int Changed_Entities = 0;
 
-            string Selected_Xml = "";           
+            string Selected_Xml = "";
+            string Xml_Directory = Properties.Settings.Default.Xml_Directory;
             string Entity_Name = Wash_String(Combo_Box_Entity_Name.Text);
             string Selected_Tag = Regex.Replace(Combo_Box_Tag_Name.Text, "[\n\r\t </>]", ""); // Also removing </> tag values
             string Selected_Type = Wash_String(Combo_Box_Type_Filter.Text);
 
-            XElement Selected_Instance = null;
+            // XElement Selected_Instance = null;
             IEnumerable<XElement> Instances = null;
             List <string> Changed_Xmls = new List<string>();
-            List<string> File_Collection = null;
 
-            
 
-            if (!Directory.Exists(Properties.Settings.Default.Xml_Directory))
+
+
+            if (!Directory.Exists(Xml_Directory))
             { MessageBox.Show("Can't find the Xml Directory."); return null; }
 
 
 
-            if (Apply_Changes) 
-            {
-                // Caution_Window Caution = (Caution_Window)Application.OpenForms["Caution"]; // Caution_Window();
-                foreach (string Entry in Caution_Window.Passed_Table.Content)
-                { if (Entry != "" & Entry != null) { File_Collection.Add(Entry); } }
-
-                if (File_Collection == null) { return null; }
-                iConsole(560, 600, string.Join("\n", File_Collection)); 
-            }
-            else { File_Collection = Get_Xmls(); }
+            if (Apply_Changes) // File_Collection is a global variable, feeded from the remaining filenames in the Caution_Window 
+            {   if (File_Collection == null | File_Collection.Count == 0) { File_Collection = Get_Xmls(); } // Failsafe
+                // iConsole(560, 600, Xml_Directory + string.Join("\n", File_Collection)); // return null; // Debug Code
+            } else { File_Collection = Get_Xmls(); }
 
 
 
@@ -440,8 +439,11 @@ namespace Log_Compactor
             {   try
                 {
                     Selected_Xml = Xml;
+                    if (Apply_Changes) { Selected_Xml = Properties.Settings.Default.Last_File; iConsole(560, 600, Selected_Xml); }
+         
+           
                     XDocument Xml_File = XDocument.Load(Selected_Xml, LoadOptions.PreserveWhitespace);
-
+                    
                     // ===================== Opening Xml File =====================
 
                     if (Entity_Name == "Multi") // Select Multiple by Name
@@ -494,7 +496,7 @@ namespace Log_Compactor
 
                                 if (Instance.Descendants(Selected_Tag).Any()) // Set the new tag value(s)
                                 {
-                                    Temporal_A = Selected_Xml.Replace(Properties.Settings.Default.Xml_Directory + @"\", ""); // Removing Path
+                                    Temporal_A = Selected_Xml.Replace(Xml_Directory, ""); // Removing Path
                                     if (!Changed_Xmls.Contains(Temporal_A))
                                     { Changed_Xmls.Add(Temporal_A); }
 
@@ -512,11 +514,12 @@ namespace Log_Compactor
                     }
 
                     if (Apply_Changes) { Xml_File.Save(Selected_Xml); } // MessageBox.Show("Saving to " + Xml); }
-                    if (Entity_Name == "Multi") { return Changed_Xmls; } // Exiting after the first (and only) Xml
+                    if (Entity_Name == "Multi") { return Changed_Xmls; } // Exiting after the first (and only) Xml File.
       
                 } catch {}
             }
 
+            File_Collection = new List<string>(); // Clearing for the next time
             return Changed_Xmls;      
         }
 
@@ -1035,8 +1038,7 @@ Tactical_Build_Cost_Multiplayer # Set the price to 1 for all Skirmish units.
         //===========================//
         public List<string> Get_Xmls()
         {
-            List<string> All_Xmls = new List<string>();
-            string Xml_Directory = Properties.Settings.Default.Xml_Directory;
+            List<string> All_Xmls = new List<string>();          
             string Error = "Please dragg and drop any target Xml, \nor the Xml directory of your mod into the Dropzone.";
 
             if (Xml_Directory == "" | Xml_Directory == null)
